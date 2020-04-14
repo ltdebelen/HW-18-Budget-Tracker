c61eb3e25a60fc33b9efe87b1af978aa5b9a5b43
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-04-13 20:44:51 -0500

    Updating mongodb url string for heroku add-in

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:password1@ds155631.mlab.com:55631/heroku_zn3gg3xd",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
